01863cc6f54edc18981ff6976e2125e2295e187e
ivanlecointere <ivanlecointere@gmail.com>
2024-04-01 05:18:24 -0400

    fixing padding on Dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -49,7 +49,7 @@
             </div>
         </div>
 
-        <div class="pt-12">
+        <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="px-4 sm:px-6 lg:px-8 py-4 sm:py-6 lg:py-8">
