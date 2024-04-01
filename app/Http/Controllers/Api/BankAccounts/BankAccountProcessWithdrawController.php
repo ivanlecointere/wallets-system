006279152fadc3cbcfa6bba42534f3cba4262ab7
ivanlecointere <ivanlecointere@gmail.com>
2024-04-01 00:49:57 -0400

    making logic for BankAccountProcessWithdrawController

--- app/Http/Controllers/Api/BankAccounts/BankAccountProcessWithdrawController.php
+++ app/Http/Controllers/Api/BankAccounts/BankAccountProcessWithdrawController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Api\BankAccounts;
 
+use App\Enums\TransactionType;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BankAccounts\BankAccountProcessWithdrawRequest;
+use App\Models\Address;
 use App\Models\BankAccount;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\DB;
 
 class BankAccountProcessWithdrawController extends Controller
 {
@@ -13,6 +17,33 @@ class BankAccountProcessWithdrawController extends Controller
      */
     public function __invoke(BankAccountProcessWithdrawRequest $request, BankAccount $bankAccount)
     {
+        DB::transaction(function() use($bankAccount, $request){
+            $depositsTotal = Transaction::where([
+                'bank_account_id' => $bankAccount->id,
+                'type' => TransactionType::DEPOSIT->value,
+            ])
+                ->sharedLock()
+                ->sum('amount');
 
+            $withdrawsTotal = Transaction::where([
+                'bank_account_id' => $bankAccount->id,
+                'type' => TransactionType::WITHDRAW->value,
+            ])
+                ->sharedLock()
+                ->sum('amount');
+
+            if ($depositsTotal - $withdrawsTotal < $request->amount * 100) {
+                throw new \Exception('Insufficient funds.');
+            }
+
+            $transaction = $bankAccount->transactions()->create([
+                'amount' => $request->amount,
+                'type' => TransactionType::WITHDRAW->value,
+            ]);
+
+            Address::factory()->for($transaction, 'addressable')->create();
+        });
+
+        return response()->json(['message' => 'Success.']);
     }
 }
--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Address extends Model
 {
@@ -23,4 +24,14 @@ class Address extends Model
         'addressable_id',
         'addressable_type',
     ];
+
+    /**
+     * Get the parent addressable model (bank account or transaction).
+     *
+     * @return MorphTo
+     */
+    public function addressable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
