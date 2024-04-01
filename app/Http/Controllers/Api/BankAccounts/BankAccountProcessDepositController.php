6fe6b60f2df5ee69e53be371160a23267a45683c
ivanlecointere <ivanlecointere@gmail.com>
2024-04-01 01:21:37 -0400

    making deposit controller logic

--- app/Http/Controllers/Api/BankAccounts/BankAccountProcessDepositController.php
+++ app/Http/Controllers/Api/BankAccounts/BankAccountProcessDepositController.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers\Api\BankAccounts;
 
+use App\Enums\TransactionType;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\BankAccounts\BankAccountProcessTransaction;
+use App\Models\Address;
+use App\Models\BankAccount;
 
 class BankAccountProcessDepositController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(BankAccountProcessTransaction $request, BankAccount $bankAccount)
     {
-        //
+        $transaction = $bankAccount->transactions()->create([
+            'amount' => $request->amount,
+            'type' => TransactionType::DEPOSIT->value,
+        ]);
+
+        Address::factory()->for($transaction, 'addressable')->create();
+
+        return response()->json(['message' => 'Success.']);
     }
 }
